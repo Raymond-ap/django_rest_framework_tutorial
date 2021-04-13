a93e8102504246d07afb4f55323038c0ff86bdee
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-13 04:33:24 -0700

    generate random slugs

--- api/models.py
+++ api/models.py
@@ -5,10 +5,29 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
-    
+
     class Meta:
         ordering = ['-id']
 
     def __str__(self):
+        return self.title
+
+    # Generate random slugs
+    def save(self, *args, **kwargs):
+        global str
+        if self.slug == None:
+            slug = slugify(self.title)
+
+            has_slug = Task.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.slug) + '-' + str(count)
+                has_slug = Task.objects.filter(slug=slug).exists()
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+    title = models.CharField(max_length=200)
         return self.title
     
