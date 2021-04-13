05ea98472474d0a4b4b39727c2708273e6bfe613
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-13 04:23:37 -0700

    add task model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Task(model.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+    
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.title
+    
