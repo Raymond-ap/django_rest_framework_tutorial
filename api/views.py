343ad9b8d4356d2e75b7143d57c80724c43cfffb
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-13 05:10:53 -0700

    commit

--- api/views.py
+++ api/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .serializers import TaskSerializer
+
+from .models import Task
+
+
+@api_view(['GET', 'POST'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    api_urls = {
+        'List': '/task-list',
+        'Detail view': '/task-detail/<slug:slug>/',
+        'Create': '/task-create/<slug:slug>/',
+        'Update': '/taks-update/<slug:slug>/',
+        'Delete': '/task-delete/<slug:slug>/'
+    }
+
+    return Response(api_urls)
+
+
+@api_view(['GET', 'POST'])
+def TaskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def TaskDetail(request, slug):
+    tasks = Task.objects.get(slug=slug)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def TaskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
