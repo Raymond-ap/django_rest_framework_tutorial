657f424fe5872c67da5a690f9af107f401445758
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-13 05:10:45 -0700

    commit

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', apiOverview, name="api-overview")
+    path('', apiOverview, name="api-overview"),
+    path('task-list', TaskList, name="task-list"),
+    path('task-detail/<slug:slug>/', TaskDetail, name="task-detail"),
+    path('task-create', TaskCreate, name="task-create"),
 ]
