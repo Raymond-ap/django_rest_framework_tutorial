3f08ed31bce5d6ab7503b585d307543366715669
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-13 04:31:57 -0700

    slug field

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
     
     class Meta:
         ordering = ['-id']
