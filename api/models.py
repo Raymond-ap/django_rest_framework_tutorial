79a4b788d561a7f5df33a3c45b71bdba6e6ed19a
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-13 04:25:08 -0700

    fix models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Task(model.Model):
+class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, blank=True, null=True)
     
