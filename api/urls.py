6c24567b8260f1c3702c8cafcfeba9cee18dfd46
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-14 08:43:23 -0700

    changed slug to pk

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', apiOverview, name="api-overview"),
     path('task-list', TaskList, name="task-list"),
-    path('task-detail/<slug:slug>/', TaskDetail, name="task-detail"),
+    path('task-detail/<int:pk>/', TaskDetail, name="task-detail"),
     path('task-create', TaskCreate, name="task-create"),
+    path('task-delete/<int:pk>', TaskDelete, name="task-delete"),
 ]
